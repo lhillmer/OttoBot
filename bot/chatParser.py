6bc8e82c8508901553075e3b1d2b9bc8c90f776f
Lukas Hillmer <lukas.hillme@gmail.com>
2017-05-02 21:31:59 -0400

    updating command prefix, adding case sensitive option to commands, updated watch command with slightly more options

--- bot/chatParser.py
+++ bot/chatParser.py
@@ -6,22 +6,34 @@ _logger = logging.getLogger()
 class CommandType(Enum):
 	STARTS_WITH = 1
 	CONTAINS = 2
+	EQUALS = 3
 
 class Command():
 	"""This represents a command that the bot will respond to. Eventually, this should be able to hand further more complicated actions. For now, it just matches responses to """
-	def __init__(self, cmd_type, to_match, responses, actions):
+	def __init__(self, cmd_type, case_sensitive, to_match, responses, actions):
 		if not isinstance(cmd_type, CommandType):
 			raise TypeError("cmd_type must be an enum CommandType")
 		self.cmd_type = cmd_type
-		self.to_match = to_match
+		self.case_sensitive = case_sensitive
+		
+		if case_sensitive:
+			self.to_match = to_match
+		else:
+			self.to_match = to_match.upper()
+		
 		self.responses = responses
 		self.actions = actions
 	
 	def does_match(self, input):
+		if  not self.case_sensitive:
+			input = input.upper()
+		
 		if self.cmd_type is CommandType.STARTS_WITH:
 			return input.startswith(self.to_match)
 		elif self.cmd_type is CommandType.CONTAINS:
 			return self.to_match in input
+		elif self.cmd_type is CommandType.EQUALS:
+			return self.to_match == input
 
 class ChatParser():
 	
@@ -54,19 +66,47 @@ def add(message):
 		result = "I know, the answer is {}!".format(str(total))
 	return result
 
+def getCrawlLink(message):
+	split = message.content.split(" ")
+	result = None
+	players = {
+			"BackslashEcho": "http://crawl.akrasiac.org:8080/#watch-BackslashEcho",
+			"OttoTonsorialist": "http://crawl.akrasiac.org:8080/#watch-gimp",
+			"Tab": "http://crawl.akrasiac.org:8080/#watch-tab"
+		}
+	if len(split) == 1:
+		result = "You can't watch no one!"
+	else:
+		result = players.get(split[1], "{}?? That person doesn't even play crawl!".format(split[1]))
+	
+	return result
+
 """for now, curating the commands, because it's easier that way"""
 
 commands = [
 	Command(CommandType.STARTS_WITH,
-			"^hi",
+			False,
+			"$hi",
 			["Hello, I am OttoBot"],
 			[]),
 	Command(CommandType.CONTAINS,
+			True,
 			"Otto",
 			["Did you mean me?", "No, you probably wanted OttoTonsorialist", "It's ok, I'll go"],
 			[]),
 	Command(CommandType.STARTS_WITH,
-			"^add",
+			False,
+			"$add",
 			["I'm about to try and add some numbers",0, "That was fun!"],
 			[add]),
+	Command(CommandType.STARTS_WITH,
+			False,
+			"$watch",
+			[0],
+			[getCrawlLink]),
+	Command(CommandType.EQUALS,
+			False,
+			"$rollout",
+			["Fuck you"],
+			[])
 ]
