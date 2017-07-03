e4ae51292891de432ad3e8b95de2088427e0dd64
Lukas Hillmer <lukas.hillmer@gmail.com>
2017-07-03 19:35:19 +0000

    adding option to read token from file

--- bot/main.py
+++ bot/main.py
@@ -17,71 +17,83 @@ _logger.addHandler(stdout)
 
 """what's the difference between these two?"""
 if hasattr(asyncio, "async"):
-	ensure_future = asyncio.async
+    ensure_future = asyncio.async
 else:
-	ensure_future = asyncio.ensure_future
+    ensure_future = asyncio.ensure_future
 
 class OttoBot:
-	
-	def __init__(self, token):
+    
+    def __init__(self, token):
 
-		self.loop = asyncio.get_event_loop()
-		self.web = WebWrapper(self.loop)
-		self.discord = DiscordWrapper(token, self.web)
-		self.discord_task = None
-		self.web_task = None
-		self.shutdown_error = False
-	
-	def start(self):
-		_logger.info("Starting OttoBot")
-		
-		def begin_shutdown(signame):
-			is_error = True if signame == "SIGTERM" else False
-			msg = "Shutting down bot due to signal: {}".format(signame)
-			if is_error:
-				_logger.error(msg)
-			else:
-				_logger.info(msg)
-			self.stop(is_error)
-			
-		
-		"""make sure the primary loop is interruptable"""
-		for signame in ("SIGINT", "SIGTERM"):
-			"""windows doesn't have signals, so this will error if running on windows"""
-			try:
-				self.loop.add_signal_handler(getattr(signal, signame),functools.partial(begin_shutdown, signame))
-			except NotImplementedError:
-				pass
+        self.loop = asyncio.get_event_loop()
+        self.web = WebWrapper(self.loop)
+        self.discord = DiscordWrapper(token, self.web)
+        self.discord_task = None
+        self.web_task = None
+        self.shutdown_error = False
+    
+    def start(self):
+        _logger.info("Starting OttoBot")
+        
+        def begin_shutdown(signame):
+            is_error = True if signame == "SIGTERM" else False
+            msg = "Shutting down bot due to signal: {}".format(signame)
+            if is_error:
+                _logger.error(msg)
+            else:
+                _logger.info(msg)
+            self.stop(is_error)
+            
+        
+        """make sure the primary loop is interruptable"""
+        for signame in ("SIGINT", "SIGTERM"):
+            """windows doesn't have signals, so this will error if running on windows"""
+            try:
+                self.loop.add_signal_handler(getattr(signal, signame),functools.partial(begin_shutdown, signame))
+            except NotImplementedError:
+                pass
 
-		self.discord_task = ensure_future(self.discord.start())
-		self.web_task = ensure_future(self.web.run())
-		
-		try:
-			self.loop.run_until_complete(self.process())
-		except asyncio.CancelledError:
-			pass
-		
-		self.loop.close()
-		sys.exit(self.shutdown_error)
-	
-	def stop(self, is_error=False):
-		_logger.info("Stopping OttoBot")
-		self.shutdown_error = is_error
-		
-		if self.discord_task and not self.discord_task.done():
-			ensure_future(self.discord.disconnect(True))
-	
-	async def process(self):
-		
-		while True:
-			await asyncio.wait([self.web_task, self.discord_task], return_when=asyncio.FIRST_COMPLETED)
+        self.discord_task = ensure_future(self.discord.start())
+        self.web_task = ensure_future(self.web.run())
+        
+        try:
+            self.loop.run_until_complete(self.process())
+        except asyncio.CancelledError:
+            pass
+        
+        self.loop.close()
+        sys.exit(self.shutdown_error)
+    
+    def stop(self, is_error=False):
+        _logger.info("Stopping OttoBot")
+        self.shutdown_error = is_error
+        
+        if self.discord_task and not self.discord_task.done():
+            ensure_future(self.discord.disconnect(True))
+    
+    async def process(self):
+        
+        while True:
+            await asyncio.wait([self.web_task, self.discord_task], return_when=asyncio.FIRST_COMPLETED)
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-t", dest="discordToken", help="discord bot token")
-	args = parser.parse_args()
-	
-	bot = OttoBot(args.discordToken)
-	bot.start()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", dest="discordToken", help="discord bot token", required=False)
+    parser.add_argument("-c", dest="tokenFile", help="token file", required=False)
+    args = parser.parse_args()
+
+    token = None
+    if args.discordToken:
+        token = args.discordToken
+    elif args.tokenFile:
+        f = open(args.tokenFile, 'r')
+        token = f.read().strip()
+        f.close()
+
+    if token:
+        bot = OttoBot(token)
+        bot.start()
+    else:
+        print("Couldn't get token!")
 
 main()
