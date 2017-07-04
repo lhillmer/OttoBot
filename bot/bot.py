3ed39bcd5a0dccfca27bd1f4f503827f292c8d73
Lukas Hillmer <lukas.hillmer@gmail.com>
2017-07-04 15:27:59 +0000

    Allowing text-only commands to be saved/loaded, and added commands to create/delete text-only commands

--- bot/bot.py
+++ bot/bot.py
@@ -6,109 +6,109 @@ import asyncio
 import logging
 
 """i'm copying a ton of code from here:
-	https://github.com/gammafunk/Cerebot/blob/master/cerebot/discord.py
-	i totally don't understand most of it
+    https://github.com/gammafunk/Cerebot/blob/master/cerebot/discord.py
+    i totally don't understand most of it
 """
 
 _logger = logging.getLogger()
 
 """what's the difference between these two?"""
 if hasattr(asyncio, "async"):
-	ensure_future = asyncio.async
+    ensure_future = asyncio.async
 else:
-	ensure_future = asyncio.ensure_future
+    ensure_future = asyncio.ensure_future
 
 class DiscordWrapper(discord.Client):
-	def __init__(self, token, webWrapper, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.ping_task = None
-		self.token = token
-		self.chat_parser = chatParser.ChatParser(chatParser.commands)
-		self.webWrapper = webWrapper
+    def __init__(self, token, webWrapper, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ping_task = None
+        self.token = token
+        self.chat_parser = chatParser.ChatParser()
+        self.webWrapper = webWrapper
 
-		#add discord-specific chat commands here
-		self.chat_parser.addCommand(
-			chatParser.Command(
-				chatParser.CommandType.EQUALS,
-				False,
-				"$clear",
-				[self.clearChat])
-			)
+        #add discord-specific chat commands here
+        self.chat_parser.addCommand(
+            chatParser.Command(
+                chatParser.CommandType.EQUALS,
+                False,
+                                False,
+                "$clear",
+                [self.clearChat])
+            )
 
-	async def clearChat(self, message, web):
-		try:
-			if message.server and not message.channel.permissions_for(message.server.me).manage_messages:
-				return "OttoBot has insufficient permissions to clear this channel"
-		except Exception as e:
-			_logger.error("Failed to get permissions for clear command. Assuming bot does not have permissions")
-			return "OttoBot has insufficient permissions to clear this channel"
+    async def clearChat(self, message, web):
+        try:
+            if message.server and not message.channel.permissions_for(message.server.me).manage_messages:
+                return "OttoBot has insufficient permissions to clear this channel"
+        except Exception as e:
+            _logger.error("Failed to get permissions for clear command. Assuming bot does not have permissions")
+            return "OttoBot has insufficient permissions to clear this channel"
 
-		def is_not_pinned(m):
-			return not m.pinned
+        def is_not_pinned(m):
+            return not m.pinned
 
-		await self.purge_from(message.channel, check=is_not_pinned)
+        await self.purge_from(message.channel, check=is_not_pinned)
 
-		return "Fresh chat for ya!"
+        return "Fresh chat for ya!"
 
-	
-	def log_exception(self, e, error_msg):
-		error_reason = type(e).__name
-		if e.args:
-			error_reason = "{}: {}".format(error_reason, e.args[0])
-		_logger.error("Discord Error: %s: %s", error_msg, error_reason)
-	
-	"""what is the point of this? I'm assuming it's some sort of keep-alive, but idfk bro"""
-	async def start_ping(self):
-		while True:
-			if self.is_closed:
-				return
-			
-			try:
-				await self.ws.ping()
-			
-			except asyncio.CancelledError:
-				return
-			
-			except Exception as e:
-				self.log_exception(e, "Unable to send ping")
-				ensure_future(self.disconnect())
-				return
-			
-			await asyncio.sleep(10)
-			
-	
-	async def on_message(self, message):
-		try:
-			if message.server and not message.channel.permissions_for(message.server.me).send_messages:
-				return
-		except Exception as e:
-			_logger.error("Failed to get permissions for bot user. Assuming the bot has permissions")
-			
-		if isinstance(message.author, discord.Member) and self.user != message.author:
-			_logger.info("Received message: %s", message.content)
-			async for reply in self.chat_parser.get_replies(message, self.webWrapper):
-				if not reply or len(reply) == 0:
-					_logger.info("received empty string from yield. continuing...")
-					continue
-				else:
-					_logger.info("received from yield %s", reply)
-					await self.send_message(message.channel, reply)
+    
+    def log_exception(self, e, error_msg):
+        error_reason = type(e).__name
+        if e.args:
+            error_reason = "{}: {}".format(error_reason, e.args[0])
+        _logger.error("Discord Error: %s: %s", error_msg, error_reason)
+    
+    """what is the point of this? I'm assuming it's some sort of keep-alive, but idfk bro"""
+    async def start_ping(self):
+        while True:
+            if self.is_closed:
+                return
+            
+            try:
+                await self.ws.ping()
+            
+            except asyncio.CancelledError:
+                return
+            
+            except Exception as e:
+                self.log_exception(e, "Unable to send ping")
+                ensure_future(self.disconnect())
+                return
+            
+            await asyncio.sleep(10)
+            
+    
+    async def on_message(self, message):
+        try:
+            if message.server and not message.channel.permissions_for(message.server.me).send_messages:
+                return
+        except Exception as e:
+            _logger.error("Failed to get permissions for bot user. Assuming the bot has permissions")
+            
+        if isinstance(message.author, discord.Member) and self.user != message.author:
+            async for reply in self.chat_parser.get_replies(message, self.webWrapper):
+                if not reply or len(reply) == 0:
+                    _logger.info("received empty string from yield. continuing...")
+                    continue
+                else:
+                    _logger.info("received from yield %s", reply)
+                    await self.send_message(message.channel, reply)
 
-	
-	"""this will probably make sense once I understand ensure_future and start_ping"""
-	async def on_ready(self):
-		self.ping_task = ensure_future(self.start_ping())
-	
-	async def start(self):
-		await self.login(self.token)
-		await self.connect()
-	
-	async def disconnect(self):
-		
-		if self.ping_task and not self.ping_task.done():
-			self.ping_task.cancel()
-		
-		try:
-			await self.close()
-		except Exception as e:
-			self.log_exception(e, "Error when disconnecting")
+    
+    """this will probably make sense once I understand ensure_future and start_ping"""
+    async def on_ready(self):
+        self.ping_task = ensure_future(self.start_ping())
+    
+    async def start(self):
+        await self.login(self.token)
+        await self.connect()
+    
+    async def disconnect(self):
+        
+        if self.ping_task and not self.ping_task.done():
+            self.ping_task.cancel()
+        
+        try:
+            await self.close()
+        except Exception as e:
+            self.log_exception(e, "Error when disconnecting")
--- bot/chatParser.py
+++ bot/chatParser.py
@@ -1,140 +1,213 @@
 import webWrapper
+import pickle
 
 import logging
 from enum import Enum
 
 _logger = logging.getLogger()
+_commandsFile = "commands.pickle"
 
 class CommandType(Enum):
-	STARTS_WITH = 1
-	CONTAINS = 2
-	EQUALS = 3
+    STARTS_WITH = 1
+    CONTAINS = 2
+    EQUALS = 3
 
 class Command():
-	"""This represents a command that the bot will respond to. Eventually, this should be able to hand further more complicated actions. For now, it just matches responses to """
-	def __init__(self, cmd_type, case_sensitive, to_match, responses):
-		if not isinstance(cmd_type, CommandType):
-			raise TypeError("cmd_type must be an enum CommandType")
-		self.cmd_type = cmd_type
-		self.case_sensitive = case_sensitive
-		
-		if case_sensitive:
-			self.to_match = to_match
-		else:
-			self.to_match = to_match.upper()
-		
-		self.responses = responses
-	
-	def does_match(self, input):
-		if  not self.case_sensitive:
-			input = input.upper()
-		
-		if self.cmd_type is CommandType.STARTS_WITH:
-			return input.startswith(self.to_match)
-		elif self.cmd_type is CommandType.CONTAINS:
-			return self.to_match in input
-		elif self.cmd_type is CommandType.EQUALS:
-			return self.to_match == input
+    """This represents a command that the bot will respond to. Eventually, this should be able to hand further more complicated actions. For now, it just matches responses to """
+    def __init__(self, cmd_type, case_sensitive, removable, to_match, responses):
+        if not isinstance(cmd_type, CommandType):
+            raise TypeError("cmd_type must be an enum CommandType")
+        self.cmd_type = cmd_type
+        self.case_sensitive = case_sensitive
+        self.removable = removable
+        
+        if case_sensitive:
+            self.to_match = to_match
+        else:
+            self.to_match = to_match.upper()
+        
+        self.responses = responses
+    
+    def does_match(self, input):
+        if not self.case_sensitive:
+            input = input.upper()
+        
+        if self.cmd_type is CommandType.STARTS_WITH:
+            return input.startswith(self.to_match)
+        elif self.cmd_type is CommandType.CONTAINS:
+            return self.to_match in input
+        elif self.cmd_type is CommandType.EQUALS:
+            return self.to_match == input
 
 class ChatParser():
+    def __init__(self):
+                self.commands = []
+                self.specialCommands = []
+                try:
+                    f = open(_commandsFile, "rb")
+                    self.commands = pickle.load(f)
+                    f.close()
+                    print("loaded # of commands: " + str(len(self.commands)))
+                except Exception as e:
+                    print("Couldn't load commands: " + str(e))
 
-	def __init__(self, commands):
-		self.commands = commands
-	
-	async def get_replies(self, message, web):
-		"""this yields strings until it has completed its reply"""
-		
-		for cmd in self.commands:
-			if cmd.does_match(message.content):
-				_logger.info("Matched %s to command %s", message.content, cmd.to_match)
-				for response in cmd.responses:
-					if isinstance(response, str):
-						yield response
-					elif callable(response):
-						yield await response(message, web)
-					else:
-						raise TypeError("invalid value in responses: " + str(response))
+                self.addCommand(Command(CommandType.STARTS_WITH,
+                    False,
+                    False,
+                    "$add",
+                    ["I'm about to add some numbers", add, "That was fun!"]))
 
-	def addCommand(self, cmd):
-		if not isinstance(cmd, Command):
-			raise TypeError("cmd must be a Command object")
-		self.commands.append(cmd)
+                self.addCommand(Command(CommandType.STARTS_WITH,
+                    False,
+                    False,
+                    "$createCommand",
+                    [add_command]))
 
-async def add(message, web):
-	split = message.content.split(" ")
-	result = None
-	total = 0
-	for i in range(1, len(split)):
-		try:
-			total += int(split[i])
-		except ValueError:
-			result = "I can only add numbers, bub"
-			break
-		except Exception:
-			result = "I don't even know what's going on anymore"
-			break
-	if not result:
-		result = "I know, the answer is {}!".format(str(total))
-	return result
+                self.addCommand(Command(CommandType.STARTS_WITH,
+                    False,
+                    False,
+                    "$deleteCommand",
+                    [remove_command]))
 
-async def getCrawlLink(message, web):
-	split = message.content.split(" ")
-	result = None
-	if len(split) == 1:
-		result = "You can't watch no one!"
-	else:
-		_logger.info("about to test for existence of crawl user: " + split[1])
-		exists = await web.doesCrawlUserExist(split[1])
-		_logger.info("crawl user " + split[1] + " exists: " + str(exists))
-		if exists:
-			result = "http://crawl.akrasiac.org:8080/#watch-" + split[1]
-		else:
-			result = split[1] + "?? That person doesn't even play crawl!"
-	
-	return result
+                self.addCommand(Command(CommandType.EQUALS,
+                    False,
+                    False,
+                    "$watch",
+                    [getCrawlLink]))
+                
+                self.addCommand(Command(CommandType.EQUALS,
+                    False,
+                    False,
+                    "$dumpLink",
+                    [getCrawlDumpLink]))
+                
+    async def get_replies(self, message, web):
+        """this yields strings until it has completed its reply"""
 
-async def getCrawlDumpLink(message, web):
-	split = message.content.split(" ")
-	result = None
-	if len(split) == 1:
-		result = "You can't watch no one!"
-	else:
-		if await web.doesCrawlUserExist(split[1]):
-			result = "http://crawl.akrasiac.org/rawdata/{}/{}.txt".format(split[1], split[1])
-		else:
-			result = split[1] + "?? That person doesn't even play crawl!"
-	
-	return result
+        for cmd in self.commands:
+            if cmd.does_match(message.content):
+                _logger.info("Matched %s to command %s", message.content, cmd.to_match)
+                for response in cmd.responses:
+                    if isinstance(response, str):
+                        yield response
+                    elif callable(response):
+                        yield await response(message, web, self)
+                    else:
+                        raise TypeError("invalid value in responses: " + str(response))
 
-"""for now, curating the commands, because it's easier that way"""
+        for cmd in self.specialCommands:
+            if cmd.does_match(message.content):
+                _logger.info("Matched %s to command %s", message.content, cmd.to_match)
+                for response in cmd.responses:
+                    if isinstance(response, str):
+                        yield response
+                    elif callable(response):
+                        yield await response(message, web, self)
+                    else:
+                        raise TypeError("invalid value in responses: " + str(response))
 
-commands = [
-	Command(CommandType.STARTS_WITH,
-			False,
-			"$hi",
-			["Hello, I am OttoBot"]),
-	Command(CommandType.STARTS_WITH,
-			False,
-			"$add",
-			["I'm about to try and add some numbers",add, "That was fun!"]),
-	Command(CommandType.STARTS_WITH,
-			False,
-			"$watch",
-			[getCrawlLink]),
-	Command(CommandType.EQUALS,
-			False,
-			"$rollout",
-			["Fuck you"]),
-	Command(CommandType.STARTS_WITH,
-			False,
-			"$dumpLink",
-			[getCrawlDumpLink]),
-        Command(CommandType.EQUALS,
-                        False,
-                        "$stallman",
-                        ["Have you tried installing a Free as in Free Speech operating system? Proprietary software may be poisoning your system."]),
-        Command(CommandType.EQUALS,
-                        False,
-                        "$sexyStallman",
-                        ["D'oh! Stupid sexy Stallman!", "https://rms.sexy"])
-]
+    def addCommand(self, cmd):
+        if not isinstance(cmd, Command):
+            raise TypeError("cmd must be a Command object")
+        specialCommand = False
+        for response in cmd.responses:
+            if not isinstance(response, str):
+                specialCommand = True
+                break
+
+        if specialCommand:
+            self.specialCommands.append(cmd)
+        else:
+            self.commands.append(cmd)
+
+    def save_commands(self):
+        _logger.info("starting save!")
+        _logger.info("type of self.commands: " + str(type(self.commands)))
+        try:
+            f = open(_commandsFile, "wb")
+            _logger.info("opened file: " + _commandsFile)
+            pickle.dump(self.commands, f)
+            _logger.info("dumped object to file")
+            f.close()
+            _logger.info("done!")
+        except Exception as e:
+            _logger.info("couldn't save commands: " + str(e))
+
+
+
+async def add(message, web, parser):
+    split = message.content.split(" ")
+    result = None
+    total = 0
+    for i in range(1, len(split)):
+        try:
+            total += int(split[i])
+        except ValueError:
+            result = "I can only add numbers, bub"
+            break
+        except Exception:
+            result = "I don't even know what's going on anymore"
+            break
+    if not result:
+        result = "I know, the answer is {}!".format(str(total))
+    return result
+
+async def add_command(message, web, parser):
+    split = message.content.split(" ", 2)
+    result = ""
+    try:
+        parser.addCommand(Command(CommandType.EQUALS,
+                False,
+                True,
+                split[1],
+                [split[2]]))
+        result = "Added command: " + split[1]
+        parser.save_commands()
+    except Exception as e:
+        result = "Failed to add command: " + str(e)
+
+    return result
+
+async def remove_command(message, web, parser):
+    split = message.content.split(" ")
+    result = ""
+    for c in parser.commands:
+        if c.does_match(split[1]):
+            if c.removable:
+                parser.commands.remove(c)
+                parser.save_commands()
+                result = "Removed command: " + c.to_match
+                break
+            else:
+                result = "Command not removable"
+                break
+    return result
+
+async def getCrawlLink(message, web, parser):
+    split = message.content.split(" ")
+    result = None
+    if len(split) == 1:
+        result = "You can't watch no one!"
+    else:
+        _logger.info("about to test for existence of crawl user: " + split[1])
+        exists = await web.doesCrawlUserExist(split[1])
+        _logger.info("crawl user " + split[1] + " exists: " + str(exists))
+        if exists:
+            result = "http://crawl.akrasiac.org:8080/#watch-" + split[1]
+        else:
+            result = split[1] + "?? That person doesn't even play crawl!"
+    
+    return result
+
+async def getCrawlDumpLink(message, web, parser):
+    split = message.content.split(" ")
+    result = None
+    if len(split) == 1:
+        result = "You can't watch no one!"
+    else:
+        if await web.doesCrawlUserExist(split[1]):
+            result = "http://crawl.akrasiac.org/rawdata/{}/{}.txt".format(split[1], split[1])
+        else:
+            result = split[1] + "?? That person doesn't even play crawl!"
+    
+    return result
