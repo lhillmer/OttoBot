734445b62c6b0bbde9f096970fb4e47822d1b837
Lukas Hillmer <lukas.hillmer@gmail.com>
2021-02-20 18:15:13 -0500

    forcing capitalization for crypto symbols

--- bot/cryptoConverter.py
+++ bot/cryptoConverter.py
@@ -11,6 +11,7 @@ class CryptoConverter():
         self.rest = RestWrapper(webWrapper, "https://pro-api.coinmarketcap.com", {'CMC_PRO_API_KEY': api_key})
 
     async def get_info(self, crypto_symbol):
+        crypto_symbol = crypto_symbol.upper()
         response = await self.rest.request("/v1/cryptocurrency/quotes/latest", {'symbol': crypto_symbol})
         parsed_response = json.loads(await response.text())
         try:
@@ -21,8 +22,8 @@ class CryptoConverter():
                 "Marketcap": StockInfo.get_wordy_num(main_data['quote']['USD']['market_cap']),
             }
         except Exception as e:
-            result = "Error occurred parsing crypto data: " + str(e)
-            _logger.error(result)
+            result = str(e)
+            _logger.error("Error occurred parsing crypto data: " + result)
             _logger.exception(e)
         return result
     
